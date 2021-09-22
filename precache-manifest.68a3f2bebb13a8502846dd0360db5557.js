fee7accea802789aed0fd5f4cdf26359a3990b61
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-09-22 00:18:43 -0300

    Updates

---
+++ precache-manifest.68a3f2bebb13a8502846dd0360db5557.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66d7a01e8279c67bc259dc2ff8a5bae4",
+    "url": "/starwars-planets/index.html"
+  },
+  {
+    "revision": "1da4d447996c8ecf4e35",
+    "url": "/starwars-planets/static/css/main.9709d3dc.chunk.css"
+  },
+  {
+    "revision": "971f84054aa607b2a6ed",
+    "url": "/starwars-planets/static/js/2.accf6ddb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planets/static/js/2.accf6ddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1da4d447996c8ecf4e35",
+    "url": "/starwars-planets/static/js/main.c680db87.chunk.js"
+  },
+  {
+    "revision": "d9d282393e64bbdbf77e",
+    "url": "/starwars-planets/static/js/runtime-main.9360541a.js"
+  },
+  {
+    "revision": "633a58ef8faabc3b622e91db8e510350",
+    "url": "/starwars-planets/static/media/bUlkrc.633a58ef.jpg"
+  }
+]);
